3ffa80f97c396d9ce567190d97e39828eff1e42f
Madebykrol <madebykrol@gmail.com>
2013-07-20 02:15:45 +0200

    Alpha release

--- src/controllers/BasicController.php
+++ src/controllers/BasicController.php
@@ -4,18 +4,25 @@ use smll\cms\framework\content\PageReference;
 
 use smll\cms\framework\PageController;
 use src\content\pages\BasicPagePage;
+use smll\cms\framework\content\utils\interfaces\IContentRepository;
 /**
  * @author ksdkrol
  * [ContentType(BasicPage)]
  */
 class BasicController extends PageController {
 	
+	private $contentRepository = null;
+	
+	public function __construct(IContentRepository $contentRepository) {
+		$this->contentRepository = $contentRepository;
+	}
+	
 	/**
 	 * @return ViewResult
 	 */
 	public function index(PageReference $currentPage) {
 		$this->viewBag['title'] = $currentPage->getTitle();
-    return $this->view($currentPage);
+		return $this->view($this->contentRepository->getPageData($currentPage));
 	}
 	
 }
