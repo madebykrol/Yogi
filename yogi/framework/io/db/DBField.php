d3aebd10278ae81315f66d3ddd64a31addb9fa63
Kristoffer Olsson <Kristoffer.olsson@ninetech.com>
2016-06-03 11:06:14 +0200

    stash changes to DBField

--- yogi/framework/io/db/DBField.php
+++ yogi/framework/io/db/DBField.php
@@ -1,5 +1,6 @@
 <?php
 namespace yogi\framework\io\db;
+
 class DBField {
 	protected $name;
 	protected $type;
@@ -7,11 +8,17 @@ class DBField {
 	protected $unique;
 	protected $primary;
 	protected $autoIncrement;
+	protected $default;
+	protected $null;
 	
-	public function __construct($name, $type, $length = null, $unique = false, $primary = false, $autoIncrement = false) {
+	public function __construct($name, $type, $default, $null = false, $length = null, $unique = false, $primary = false, $autoIncrement = false) {
 		$this->name = $name;
 		$this->type = $type;
+		$this->default = $default;
 		
+		if(is_boolean($null)) {
+			$this->null = $null;
+		}
 		if(is_numeric($length)) {
 			$this->lenght = $length;
 		}
@@ -24,6 +31,40 @@ class DBField {
 		if (is_boolean($this->autoIncrement)) {
 			$this->autoIncrement = $autoIncrement;
 		}
+	}
+	
+	public static function fromObjProperty(\ReflectionProperty $property) {
 		
+		
+		return new DBField();
+	}
+	
+	public static function fromDbColumn($dbColumn) {
+		
+		$primary 		= false;
+		$null 			= false;
+		$autoIncrement 	= false;
+		$unique 		= false;
+		$name 		= "";
+		$type 		= "";
+		$lenght		= "";
+		
+		if(isset($dbColumn->Key)) {
+			$primary = $dbColumn->Key == "PRI";
+		}
+		if(isset($dbColumn->Null)) {
+			$null = $dbColumn->Null == "NO";
+		}
+		if(isset($dbColumn->Extra)) {
+			$autoIncrement = $dbColumn->Extra == "auto_increment";
+		}
+		if(isset($dbColumn->Field)) {
+			$name = $dbColumn->Field;
+		}
+		if(isset($dbColumn->Type)) {
+			
+		}
+		
+		return new DBField($name, $type, $default, $null, $length, $unique, $primary, $autoIncrement);
 	}
 }
